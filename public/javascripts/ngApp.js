de194aabb21f0750a2d11f89327b9fe3b87fae40
Zack Claar <claar.zack@gmail.com>
2015-10-01 15:56:48 -0600

    Setting up the callback URI and the callback route in order to catch the URL before html5mode takes away the # sign before token. After that, and the cookie is stored, will be working on the account page

--- public/javascripts/ngApp.js
+++ public/javascripts/ngApp.js
@@ -1,1 +1,17 @@
 var app = angular.module('YRAlerts', ['ui.router', 'ngCookies']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  //making 'pretty urls'
+  $locationProvider.html5Mode(true);
+
+  //for urls not recognized
+  $urlRouterProvider.otherwise('/');
+
+  //setting up the states
+  $stateProvider
+    .state('welcome', {
+      url: '/',
+      templateUrl: 'ngPartials/welcome.html'
+    });
+});
--- public/javascripts/ngController.js
+++ public/javascripts/ngController.js
@@ -2,3 +2,8 @@ app.controller('NavbarController',['$scope', '$http',
 function($scope, $http) {
   console.log('in navbar controller');
 }]);
+
+app.controller('WelcomeController', ['$scope', '$http',
+function($scope, $http) {
+  console.log('in welcome controller');
+}])
